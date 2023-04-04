f79759d83d1eb308654fc23d12cabed92fa7415d
azazana <azazana@list.ru>
2023-04-04 23:06:11 +0300

    add models

--- Django/Django_project/first_app/admin.py
+++ Django/Django_project/first_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topic, AccessRecord, Webpage
+
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(Webpage)
